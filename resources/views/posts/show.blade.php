376b9e121df829d3f462b2ebeedc930dcefed55b
James Samalenge <james@Jamess-MacBook-Pro.local>
2021-04-05 15:43:55 +0200

    Cleaning Code

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -39,12 +39,6 @@
               <label class="star star-{{$i}} unrated" for="star-{{$i}}"></label>
             @endif
           @endfor
-          <!--        <input class="star star-3" value="3" id="star-3" type="radio" name="star"/>-->
-          <!--        <label class="star star-3" for="star-3"></label>-->
-          <!--        <input class="star star-2" value="2" id="star-2" type="radio" name="star"/>-->
-          <!--        <label class="star star-2" for="star-2"></label>-->
-          <!--        <input class="star star-1" value="1" id="star-1" type="radio" name="star"/>-->
-          <!--        <label class="star star-1" for="star-1"></label>-->
         </div>
         <div class="col-sm-4 text">
           <span>Rating: {{ (int)$rating }}</span>
